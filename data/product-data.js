6e68c698240aa96caa836665cdc22f031073f2c1
Hieu <danghieuyt2@gmail.com>
2025-10-31 21:28:58 +0700

    fix: product data

--- data/product-data.js
+++ data/product-data.js
@@ -65,48 +65,4 @@ export const product_data = [
             "Speak-to-chat technology"
         ], 
     },
-    // {
-    //     "id": 2,
-    //     "title": "Dầu Gội",
-    //     "price": 349.99,                           
-    //     "original_price": 399.99,                  
-    //     "description": "Dầu gội tinh chất vỏ bưởi", 
-    //     "rating": 4.5,                             
-    //     "review_count": 120,                        
-    //     "main_image": "product-3.png",     
-    //     "images": [                               
-    //         "store-product-4.jpg",
-    //         "store-product-5.jpg",
-    //         "store-product-6.jpg"
-    //     ],
-    //     "stock": 15,                              
-    //     "features": [                            
-    //         "Industry-leading noise cancellation",
-    //         "30-hour battery life",
-    //         "Touch sensor controls",
-    //         "Speak-to-chat technology"
-    //     ],
-    // },
-    // {
-    //     "id": 3,
-    //     "title": "Xịt Tóc",
-    //     "price": 349.99,                           
-    //     "original_price": 399.99,                  
-    //     "description": "Nước xịt dưỡng tóc tinh dầu bưởi", 
-    //     "rating": 4.5,                             
-    //     "review_count": 120,                        
-    //     "main_image": "product-4.png",     
-    //     "images": [                               
-    //         "store-product-4.jpg",
-    //         "store-product-5.jpg",
-    //         "store-product-6.jpg"
-    //     ],
-    //     "stock": 15,                              
-    //     "features": [                            
-    //         "Industry-leading noise cancellation",
-    //         "30-hour battery life",
-    //         "Touch sensor controls",
-    //         "Speak-to-chat technology"
-    //     ],  
-    // },
 ]
